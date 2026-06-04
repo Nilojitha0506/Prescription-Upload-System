4ab20c939a27484e0865d54a5dc0baf2430374a3
nilojitha-2026 <nilojitha@rubaai.net>
2026-06-04 16:39:39 +0530

    test

--- auth/login.php
+++ auth/login.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>Login</title>
+<title>Login.....!</title>
 <style>
   body {
     font-family: 'Poppins', sans-serif;
